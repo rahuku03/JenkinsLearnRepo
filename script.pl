ccd53aa410569f5044286318d6e75d72239c8ac0
rahuku03 <58826161+rahuku03@users.noreply.github.com>
2020-07-28 09:10:49 +0530

    Create script.pl

---
+++ script.pl
@@ -0,0 +1,29 @@
+#!/usr/bin/perl
+
+# This will print "Hello, World"
+print "Hello, world\n";
+print("Hello world again\n");
+$a=50;
+print "value is = $a\n";
+
+$result="This is the \"He checking the double quotes\"";
+print "$result\n";
+print "\$result\n";
+
+@name=("Rahul","Amit","Mukesh");
+print "@name";
+
+@copy=@name;
+$size=@name;
+
+print "@copy\n";
+print "$size\n";
+
+$smile  = v9786;
+$foo    = v102.111.111;
+$martin = v77.97.114.116.105.110;
+
+print "smile = $smile\n";
+print "foo = $foo\n";
+print "martin = $martin\n";
+
